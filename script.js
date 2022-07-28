f53ce8128e88ece7aef5aaff4ee66a43ae494ee2
jeremy-b <jeremy.steven.bess@gmail.com>
2022-07-28 15:31:15 -0700

    Create variable to store result of game

--- script.js
+++ script.js
@@ -15,23 +15,34 @@ function playRound(playerSelection, compSelection){
     compSelection = getComputerChoice();
     playerPreLower = prompt("Rock, Paper, or Scissors??? Choose wisely!", "")
     playerSelection = playerPreLower.toLowerCase();
+
+    let compWin = 0
+    let playerWin = 0
+    let tie = 0
     
     if (compSelection === playerSelection){
-        resultTie = "TIE! No winners, no losers. How very boring..."
+        tie += 1
+        result = "TIE! No winners, no losers. How very boring..."
     } else if (compSelection === "rock" && playerSelection === "scissors"){
-        resultCompWin = "YOU LOSE! Rock beats scissors! Embarrassing!!!"
+        compWin += 1;
+        result = "YOU LOSE! Rock beats scissors! Embarrassing!!!"
     } else if (compSelection === "rock" && playerSelection === "paper"){
-        resultPlayerWin = "you know, paper really shouldn't beat rock... it doesn't make sense... but you win. who cares."
+        playerWin += 1
+        result = "you know, paper really shouldn't beat rock... it doesn't make sense... but you win. who cares."
     } else if (compSelection === "scissors" && playerSelection === "paper"){
-        resultCompWin = "YOU LOSE!!! Wrong choice, dumbass! Scissors beats paper! PATHETIC!!!"
+        compWin += 1
+        result = "YOU LOSE!!! Wrong choice, dumbass! Scissors beats paper! PATHETIC!!!"
     } else if (compSelection === "scissors" && playerSelection === "rock"){
-        resultPlayerWin = "bruh... i guess you win... rock beats scissors but i dont even care..."
+        playerWin +=1
+        result = "bruh... i guess you win... rock beats scissors but i dont even care..."
     } else if (compSelection === "paper" && playerSelection === "rock"){
-        resultCompWin = "AAAAAH GOTTEEEM! LOSER! Paper beats rock!!! Obviously!!!"
+        compWin +=1
+        result = "AAAAAH GOTTEEEM! LOSER! Paper beats rock!!! Obviously!!!"
     } else if (compSelection === "paper" && playerSelection === "scissors"){
-        resultPlayerWin = "whatever. you win this round. scissors beat paper. good for you..."
+        compWin +=1
+        result = "whatever. you win this round. scissors beat paper. good for you..."
     }
 
-    return resultTie || resultCompWin || resultPlayerWin;
-
+    return result
 }
+
