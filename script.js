6e611a110ad5561da7e4c0579d1eb60706a0113a
jeremy-b <jeremy.steven.bess@gmail.com>
2022-07-28 13:32:22 -0700

    Write function to play one round of rock paper scissors

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function getComputerChoice(compChoice){
     return compChoice
 
 }
+
+function playRound(playerSelection, compSelection){
+    compSelection = getComputerChoice();
+    playerPreLower = prompt("Rock, Paper, or Scissors??? Choose wisely!", "")
+    playerSelection = playerPreLower.toLowerCase();
+    
+    if (compSelection === playerSelection){
+        resultTie = "TIE! No winners, no losers. How very boring..."
+    } else if (compSelection === "rock" && playerSelection === "scissors"){
+        resultCompWin = "YOU LOSE! Rock beats scissors! Embarrassing!!!"
+    } else if (compSelection === "rock" && playerSelection === "paper"){
+        resultPlayerWin = "you know, paper really shouldn't beat rock... it doesn't make sense... but you win. who cares."
+    } else if (compSelection === "scissors" && playerSelection === "paper"){
+        resultCompWin = "YOU LOSE!!! Wrong choice, dumbass! Scissors beats paper! PATHETIC!!!"
+    } else if (compSelection === "scissors" && playerSelection === "rock"){
+        resultPlayerWin = "bruh... i guess you win... rock beats scissors but i dont even care..."
+    } else if (compSelection === "paper" && playerSelection === "rock"){
+        resultCompWin = "AAAAAH GOTTEEEM! LOSER! Paper beats rock!!! Obviously!!!"
+    } else if (compSelection === "paper" && playerSelection === "scissors"){
+        resultPlayerWin = "whatever. you win this round. scissors beat paper. good for you..."
+    }
+
+    return resultTie || resultCompWin || resultPlayerWin;
+
+}
