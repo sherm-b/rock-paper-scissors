685e5a1a61bd4e109655a4106a6fb716639d6956
jeremy-b <jeremy.steven.bess@gmail.com>
2022-07-28 15:54:36 -0700

    Simplify result variable and store result value and message in array

--- script.js
+++ script.js
@@ -16,36 +16,30 @@ function playRound(playerSelection, compSelection){
     playerPreLower = prompt("Rock, Paper, or Scissors??? Choose wisely!", "")
     playerSelection = playerPreLower.toLowerCase();
 
-    let compWin = 0
-    let playerWin = 0
-    let tie = 0
+    let winValue = 0;
     
     if (compSelection === playerSelection){
-        tie += 1
         result = "TIE! No winners, no losers. How very boring..."
     } else if (compSelection === "rock" && playerSelection === "scissors"){
-        compWin += 1;
+        winValue += 1;
         result = "YOU LOSE! Rock beats scissors! Embarrassing!!!"
     } else if (compSelection === "rock" && playerSelection === "paper"){
-        playerWin += 1
+        winValue += 2
         result = "you know, paper really shouldn't beat rock... it doesn't make sense... but you win. who cares."
     } else if (compSelection === "scissors" && playerSelection === "paper"){
-        compWin += 1
+        winValue += 1
         result = "YOU LOSE!!! Wrong choice, dumbass! Scissors beats paper! PATHETIC!!!"
     } else if (compSelection === "scissors" && playerSelection === "rock"){
-        playerWin +=1
+        winValue += 2;
         result = "bruh... i guess you win... rock beats scissors but i dont even care..."
     } else if (compSelection === "paper" && playerSelection === "rock"){
-        compWin +=1
+        winValue +=1
         result = "AAAAAH GOTTEEEM! LOSER! Paper beats rock!!! Obviously!!!"
     } else if (compSelection === "paper" && playerSelection === "scissors"){
-        playerWin +=1
+        winValue += 2
         result = "whatever. you win this round. scissors beat paper. good for you..."
     }
 
-    return result
+    return [winValue, result]
 }
 
-function game(){
-    for
-}
